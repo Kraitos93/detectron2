8bd7b8428f23822c3c348662cb689d84140bb63c
Rune Eggers <milehigh.thedane@gmail.com>
2019-12-21 13:39:26 +0100

    changed the extension of datasets to be test

--- test_range.sh
+++ test_range.sh
@@ -6,7 +6,7 @@ cat ./csv_files/${MODEL}.csv
 python3 prepare_csv.py csv_files/${MODEL}.csv
 #Do inference of all the models
 for i in $(seq 1 $EPOCH_TO); do
-python3 train.py test ./datasets/${MODEL}_${i}_cyclegan faster_rcnn_R_50_C4_3x.yaml 137849393/model_final_f97cb7.pkl ${MODE}_bottle ${MODE} ${i}; 
+python3 train.py test ./datasets/${MODEL}_${i}_test faster_rcnn_R_50_C4_3x.yaml 137849393/model_final_f97cb7.pkl ${MODE}_bottle ${MODE} ${i}; 
 done
 #Generate the graphs
 python3 generate_graph.py ${MODEL}
