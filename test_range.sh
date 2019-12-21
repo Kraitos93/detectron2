fde9a1ae07d4532e04ee3eed3fcadc4a3af45d49
Rune Eggers <milehigh.thedane@gmail.com>
2019-12-21 13:46:25 +0100

    changed the way mode is decided

--- test_range.sh
+++ test_range.sh
@@ -6,7 +6,11 @@ cat ./csv_files/${MODEL}.csv
 python3 prepare_csv.py csv_files/${MODEL}.csv
 #Do inference of all the models
 for i in $(seq 1 $EPOCH_TO); do
-python3 train.py test ./datasets/${MODEL}_${i}_test faster_rcnn_R_50_C4_3x.yaml 137849393/model_final_f97cb7.pkl ${MODE}_bottle ${MODE} ${i}; 
+MODE_FORMAT=bottle
+if [ -z "$MODE" ]
+then
+    MODE_FORMAT=${MODE}_${MODE_FORMAT}
+python3 train.py test ./datasets/${MODEL}_${i}_test faster_rcnn_R_50_C4_3x.yaml 137849393/model_final_f97cb7.pkl ${MODE_FORMAT} ${MODE} ${i}; 
 done
 #Generate the graphs
 python3 generate_graph.py ${MODEL}
