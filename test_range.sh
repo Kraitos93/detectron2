a6da44095d6d9c228a431fd78eae26c3335baf3f
Rune Eggers <milehigh.thedane@gmail.com>
2019-12-26 16:48:35 +0100

    changed the name of csv to include mode also

--- test_range.sh
+++ test_range.sh
@@ -2,8 +2,8 @@ MODEL=$1
 EPOCH_TO=$2
 MODE=$3
 #Build the csv file
-cat ./csv_files/${MODEL}.csv
-python3 prepare_csv.py csv_files/${MODEL}.csv
+cat ./csv_files/${MODEL}_${MODE}.csv
+python3 prepare_csv.py csv_files/${MODEL}_${MODE}.csv
 #Do inference of all the models
 for i in $(seq 1 $EPOCH_TO); do
 MODE_FORMAT=bottle
@@ -11,7 +11,7 @@ if [ -z "$MODE" ]
 then
     MODE_FORMAT=${MODE}_${MODE_FORMAT}
 fi
-python3 train.py test ./datasets/${MODEL}_${i}_test faster_rcnn_R_50_C4_3x.yaml 137849393/model_final_f97cb7.pkl ${MODE_FORMAT} ${MODE} csv_files/${MODEL}.csv ${i}; 
+python3 train.py test ./datasets/${MODEL}_${i}_test faster_rcnn_R_50_C4_3x.yaml 137849393/model_final_f97cb7.pkl ${MODE_FORMAT} ${MODE} csv_files/${MODEL}_${MODE}.csv ${i}; 
 done
 #Generate the graphs
-python3 generate_graph.py ${MODEL}
+python3 generate_graph.py ${MODEL}_${MODE}
